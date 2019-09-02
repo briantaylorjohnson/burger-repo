eeca8b9548cdeb8e257b4eb7937da5089c1c25d5
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-09-01 23:36:42 -0400

    Added server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+// Required dependencies and packages
+var express = require("express");
+
+// Sets up the Express.js application
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for synchronization
+var db = require("./models");
+
+// Sets up the Express.js application to allow for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory for application assets
+app.use(express.static("public"));
+
+// Require routes HTML pages and APIs
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Synchonizes the Sequelize models and then starts up the application
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on Port: " + PORT);
+  });
+});
