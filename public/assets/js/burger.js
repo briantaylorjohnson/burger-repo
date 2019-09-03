2ee9c2a98df507c181d1df71d40429be39218025
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-09-02 21:02:49 -0400

    Added markup/Bootstrap CSS to pretty the frontend; built logic to add new burgers

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -2,77 +2,128 @@
 $(document).ready(function()
 {
     var relativeURL = window.location.origin; // Captures the relative URL for the page so that API requests can be made
-    var burgersArray = [];
 
-    $.get(relativeURL + "/burger", function(data)
+    function getBurgers()
     {
-        console.log(data);
-        burgersArray = data;
-
-        for(i = 0; i < data.length; i++)
+        $.get(relativeURL + "/burger", function(data)
         {
-            console.log(data[i].id);
-            console.log(data[i].name);
-            
-            var burgerRow = $("<div>");
-            var burgerRowId = "burger-row-" + data[i].id;
-            var burgerDiv = $("<div>");
-            var devourDiv = $("<div>");
-            var devourButton = $("<button>");
-            
-            burgerRow.attr("class",burgerRowId + " row");
-            burgerDiv.attr(
-            {
-                "class": "md-col-5 text-right mt-4 mr-4",
-                "id": data[i].id
-            });
-            devourDiv.attr(
-            {
-                "class": "md-col-1 text-right mt-4",
-                "id": data[i].id
-            });
+            console.log(data);
 
-            devourDiv.attr(
+            for(i = 0; i < data.length; i++)
             {
-                "class": "devour-btn",
-                "id": data[i].id
-            });
+                console.log(data[i].id);
+                console.log(data[i].name);
+                
+                if(data[i].devoured == false)
+                {
+                    var burgerRow = $("<div>");
+                    var burgerRowId = "burger-row-" + data[i].id;
+                    var burgerDiv = $("<div>");
+                    var devourDiv = $("<div>");
+                    var devourButton = $("<button>");
+                    
+                    burgerRow.attr("class",burgerRowId + " row pl-3 pb-3");
+                    burgerDiv.attr(
+                    {
+                        "class": "md-col-5 text-right mt-4 mr-4",
+                        "id": data[i].id
+                    });
+                    devourDiv.attr(
+                    {
+                        "class": "md-col-1 text-right mt-4",
+                        "id": data[i].id
+                    });
 
-            devourButton.text("Devour");
-            burgerDiv.append(data[i].name);
-            devourDiv.append(devourButton);
-            burgerRow.append(burgerDiv);
-            burgerRow.append(devourDiv);            
-            $(".burger-list").append(burgerRow);
-        }
+                    devourButton.attr(
+                    {
+                        "class": "devour-btn",
+                        "id": data[i].id,
+                        "burger": data[i].name
+                    });
 
+                    devourButton.text("Devour");
+                    burgerDiv.append(data[i].name);
+                    devourDiv.append(devourButton);
+                    burgerRow.append(burgerDiv);
+                    burgerRow.append(devourDiv);            
+                    $(".burger-list").append(burgerRow);
+                }
+            }
+
+            devourBurger();
+
+            addBurger();
+        });
+    }
+
+    function devourBurger()
+    {
         $(".devour-btn").on("click", function(event)
-            {
-            console.log(burgersArray);
+        {
             
             var devourId = $(this).attr("id");
-            devourId = (parseInt(devourId)) - 1;
-            var burgerRowId = "burger-row-" + burgersArray[devourId].id;
+            devourId = (parseInt(devourId));
+            var burgerRowId = "burger-row-" + $(this).attr("id");
             
             console.log(burgerRowId);
 
             var burgerRow = $("<div>");
             var burgerDiv = $("<div>");
 
-            burgerRow.attr("class","row");
+            burgerRow.attr("class","row pl-3 pb-3");
             burgerDiv.attr(
             {
                 "class": "md-col-6 text-right mt-4 mr-4",
                 "id": devourId
             });
 
-            burgerDiv.append(burgersArray[devourId].name);
+            burgerDiv.append($(this).attr("burger"));
             burgerRow.append(burgerDiv);
             $(".devoured-list").append(burgerRow);
 
             $("." + burgerRowId).remove();
 
+            $.ajax(
+            {
+                method: "PUT",
+                url: relativeURL + "/burger",
+                data:
+                {
+                    "id": devourId
+                }
+            }).done(function(data)
+            {
+                console.log("Burger Devoured!");
+                console.log(data);
+            });
         });
+    }
+
+    function addBurger()
+    {
+        $(".add-burger-btn").on("click", function(event)
+        {
+            var burgerName = $("#add-burgers-here").val();
+
+            $.ajax(
+            {
+                method: "POST",
+                url: relativeURL + "/burger",
+                data:
+                {
+                    "name": burgerName
+                }
+            }).done(function(data)
+            {
+                console.log("Burger added!");
+                console.log(data);
+
+                getBurgers();
+
+            });
+        });
+    }
+
+getBurgers();
     
-    });
 });
